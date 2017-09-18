ba9f7d2b9377145fc07a429aead2d5e27454a72e
Medhi Widjaja <medhiwidjaja@yahoo.com>
2017-09-18 07:39:49 +0700

    Cookie salt for integrating with Phoenix part

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: '_brightlight_session'
+Rails.application.config.action_dispatch.encrypted_cookie_salt = ENV['SESSION_ENCRYPTED_COOKIE_SALT']
+Rails.application.config.action_dispatch.encrypted_signed_cookie_salt = ENV['SESSION_ENCRYPTED_SIGNED_COOKIE_SALT']
