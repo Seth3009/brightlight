3b130b6ffa71939d90ae5d6682eb9f1cb4426f88
Medhi Widjaja <medhiwidjaja@yahoo.com>
2017-11-02 08:56:35 +0700

    Requisition list

--- app/controllers/requisitions_controller.rb
+++ app/controllers/requisitions_controller.rb
@@ -6,17 +6,23 @@ class RequisitionsController < ApplicationController
   def index
     authorize! :read, Requisition
     @requisitions = Requisition.all
+    if params[:dept].present? && params[:dept] != 'all'
+      dept = Department.where(code: params[:dept]).take
+      @requisitions = @requisitions.where(department: dept)
+    end
   end
 
   # GET /requisitions/1
   # GET /requisitions/1.json
   def show
     authorize! :read, @requisition
+    @req_items = @requisition.req_items
   end
 
   # GET /requisitions/new
   def new
     authorize! :create, Requisition
+    @employee = current_user.employee
     @requisition = Requisition.new
   end
 
@@ -32,7 +38,8 @@ class RequisitionsController < ApplicationController
   def create
     authorize! :create, Requisition
     @requisition = Requisition.new(requisition_params)
-
+    @requisition.created_by = current_user
+    @requisition.last_updated_by = current_user
     respond_to do |format|
       if @requisition.save
         format.html { redirect_to @requisition, notice: 'Requisition was successfully created.' }
@@ -48,6 +55,7 @@ class RequisitionsController < ApplicationController
   # PATCH/PUT /requisitions/1.json
   def update
     authorize! :update, @requisition
+    @requisition.last_updated_by = current_user
     respond_to do |format|
       if @requisition.update(requisition_params)
         format.html { redirect_to @requisition, notice: 'Requisition was successfully updated.' }
@@ -78,8 +86,16 @@ class RequisitionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def requisition_params
-      params.require(:requisition).permit(:req_no, :description, :is_budgeted, :budget_id, :budget_item_id, :date_required, :date_requested, :department_id, :requester_id, :supervisor_id, :supv_approval, :notes, :appvl_notes, :total_amt, :is_budget_approved, :is_submitted, :is_approved, :is_sent_to_supv, :is_sent_to_purchasing, :is_sent_for_bgt_approval, :is_rejected, :reject_reason, :active,
-                                          {req_items_attributes: [:requisition_id, :description, :qty_reqd, :unit, :est_price, :actual_price, :notes, :date_needed, :budgetted, :budget_item_id, :budget_name, :bdgt_approved, :bdgt_notes, :bdgt_appvl_by_id]}
+      params.require(:requisition).permit(:id, :req_no, :description, :is_budgeted, :budget_id, :budget_item_id, :budget_notes, :date_required, :date_requested, 
+                                          :department_id, :requester_id, :supervisor_id, :supv_approval, :notes, :req_appvl_notes, :total_amt, 
+                                          :is_budget_approved, :is_submitted, :is_approved, :is_sent_to_supv, :is_sent_to_purchasing, 
+                                          :is_sent_for_bgt_approval, :is_rejected, :reject_reason, :active,
+                                          :bdgt_approver_id, :bgt_appvl_notes, :purch_receiver_id, :receive_notes,
+                                          :created_by, :last_updated_by,
+                                          {req_items_attributes: [:id, :requisition_id, :description, :qty_reqd, :unit, :est_price, :actual_price, 
+                                                                  :currency, :notes, :qty_ordered, :order_date, :qty_delivered, :delivery_date,
+                                                                  :qty_accepted, :acceptance_date, :qty_rejected, :acceptance_notes, :reject_notes,
+                                                                  :needed_by_date, :id, :created_by, :last_updated_by, :_destroy]}
                                           )
     end
 end
