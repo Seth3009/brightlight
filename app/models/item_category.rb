512044f8f2f7dbf9b2aff06ec2ec3886f57b4c37
Brightlight <brightlight-cbcs@cahayabangsa.org>
2018-03-13 14:20:36 +0700

    Migration and model dependence

--- app/models/item_category.rb
+++ app/models/item_category.rb
@@ -1,5 +1,5 @@
 class ItemCategory < ActiveRecord::Base
-  has_many :products
+  has_many :products, dependent: :restrict_with_error
 
   def name_with_code
       "#{code} [#{name}]"
--- db/migrate/20180307053909_drop_supplies.rb
+++
@@ -1,5 +0,0 @@
-class DropSupplies < ActiveRecord::Migration
-  def change
-    drop_table :supplies
-  end
-end
