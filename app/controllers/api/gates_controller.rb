19ef46a936b4e2158c25af6472f0509bf5b21014
Setiadi <setiadi@cahayabangsa.org>
2018-04-12 10:54:55 +0700

    tapping response from gates_controller

--- app/controllers/api/gates_controller.rb
+++ app/controllers/api/gates_controller.rb
@@ -2,29 +2,26 @@ class Api::GatesController < Api::BaseController
 
   def create
     if params[:id].present?
-      @badge = Badge.find_by_code(params[:id])
+      @badge = Badge.where(code:params[:id]).select('badges.*').first
       if @badge.present?
         if @badge.kind = "Employee"
-          @person = Employee.joins('left join badges on badges.employee_id = employees.id')
+          @employee = Employee.joins('left join badges on badges.employee_id = employees.id')
                     .select('employees.*,badges.*').where(badges: {code: params[:id]}).first
-          if @person.present?
-            DoorAccessLog.insert_door_log(@person.employee_id, @badge.kind, params[:id],params[:loc],@person.name)
-            response.header["result"] = '{code:' + @person.code + ' name:' + @person.name + ' kind:' + @badge.kind + '}'          
-          end
-        elsif
-          @person = Student.joins('left join badges on badges.student_id = students.id')
+          if @employee.present?
+            DoorAccessLog.insert_door_log(@employee.employee_id, @employee.kind, params[:id],params[:loc],@employee.name)
+            response.header["result"] = '{code:' + @employee.code + ' name:' + @employee.name + ' kind:' + @employee.kind + '}'
+          else
+            @student = Student.joins('left join badges on badges.student_id = students.id')
                     .select('students.*,badges.*').where(badges: {code: params[:id]}).first
-          if @person.present?
-            DoorAccessLog.insert_door_log(@person.student_id, @badge.kind, params[:id],params[:loc],@person.name)
-            response.header["result"] = '{code:' + @person.code + ' name:' + @person.name + ' kind:' + @badge.kind + '}'          
+            if @student.present?
+              DoorAccessLog.insert_door_log(@student.student_id, @student.kind, params[:id],params[:loc],@student.name)
+              response.header["result"] = '{code:' + @student.code + ' name:' + @student.name + ' kind:' + @student.kind + '}'          
+            end
           end
         end
       else
         response.header["result"] = '{Invalid Card}'
-      end
-      
-    else
-      #response.header["X-result"] = '{Invalid Card}'
+      end    
     end
   end
 end
--- app/models/door_access_log.rb
+++ app/models/door_access_log.rb
@@ -3,9 +3,9 @@ class DoorAccessLog < ActiveRecord::Base
   belongs_to :student
 
   def self.insert_door_log(person_id,kind,card,location,name)
-    if kind = "Employee"
+    if kind == "Employee"
       DoorAccessLog.create(employee_id:person_id, kind:kind, card:card, location:location, name:name)
-    elsif
+    else
       DoorAccessLog.create(student_id:person_id, kind:kind, card:card, location:location, name:name)
     end
   end
