9458d820f7d9378c828e41cba02b9a419ee78b56
Medhi Widjaja <medhi.widjaja@cahayabangsa.org>
2018-04-30 13:46:56 +0700

    JS let vs var

--- app/assets/javascripts/book_copies.js
+++ app/assets/javascripts/book_copies.js
@@ -1,10 +1,10 @@
 $(document).on("ready page:load page:change", function(){
   if ($("body.book_copies").length == 0) return;
-  let toggle_menu = function() {
+  var toggle_menu = function() {
     $('.context-buttons').toggle($('input.checkbox:checked').length > 0);
     $('.nav-menu').toggle($('input.checkbox:checked').length == 0);      
   };
-  let toggle_checkbox = function() {
+  var toggle_checkbox = function() {
     toggle_menu();
     $(".allcheckboxes").prop("checked", $('input.checkbox').length == $('input.checkbox:checked').length);
   }
