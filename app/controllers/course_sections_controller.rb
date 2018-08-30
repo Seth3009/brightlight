ba2549fa438efd2321af51687c23a2df3f390e3c
Medhi Widjaja <medhi.widjaja@cahayabangsa.org>
2018-08-30 08:40:36 +0700

    Course roster editing

--- app/controllers/course_sections_controller.rb
+++ app/controllers/course_sections_controller.rb
@@ -23,7 +23,29 @@ class CourseSectionsController < ApplicationController
   # GET /course_sections/1/edit
   def edit
     authorize! :update, @course_section
-    @academic_year_id = params[:year]
+    @academic_year = params[:year] ? AcademicYear.find(params[:year]) : AcademicYear.current
+    @filterrific = initialize_filterrific(
+      Student,
+      params[:filterrific],
+      select_options: {
+        sorted_by: Student.options_for_sorted_by
+      }
+    ) or return
+
+    @students = @filterrific.find.page(params[:page])
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+
+    # Recover from invalid param sets, e.g., when a filter refers to the
+    # database id of a record that doesn’t exist any more.
+    # In this case we reset filterrific and discard all filter params.
+  rescue ActiveRecord::RecordNotFound => e
+    # There is an issue with the persisted param_set. Reset it.
+    puts "Had to reset filterrific params: #{ e.message }"
+    redirect_to(reset_filterrific_url(format: :html)) and return
   end
 
   # POST /course_sections
