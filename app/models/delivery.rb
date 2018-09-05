8e12c2c2b6d0a43058c2a665e53590b88013ed90
Medhi Widjaja <medhi.widjaja@cahayabangsa.org>
2018-09-05 12:23:52 +0700

    Purchase order views

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -4,4 +4,6 @@ class Delivery < ActiveRecord::Base
   belongs_to :checked_by, class_name: 'Employee'
   belongs_to :created_by, class_name: 'User'
   belongs_to :last_updated_by, class_name: 'User'
+
+  has_many :delivery_items
 end
