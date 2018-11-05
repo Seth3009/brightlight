63c12a920069114090598cf97dbb7b0cc9e85e84
Setiadi <setiadi@cahayabangsa.org>
2018-11-05 14:36:56 +0700

    conflicted

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20181019071836) do
-=======
 ActiveRecord::Schema.define(version: 20181026081019) do
->>>>>>> hrd
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
