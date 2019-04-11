5cfb1ff848fa1422492f4a66d768189eefff2cc6
Medhi Widjaja <medhi.widjaja@cahayabangsa.org>
2019-04-11 13:43:14 +0700

    Requisition emailer

---
+++ app/mailers/requisition_emailer.rb
@@ -0,0 +1,17 @@
+class RequisitionEmailer < ActionMailer::Base
+  default from: "Brightlight <brightlight@cahayabangsa.org>"
+
+  def approval(requisition:, level: 1)
+    @requisition = requisition
+    @type = type
+    @addressee = @requisition.approvables.level(level).map { |approver| %("#{approver.name}" <#{approver.email}>) }
+    mail(to: @addressee, subject: "Approval required: Purchase Request #{requisition.id}.")
+  end
+
+  def po_processed(requisition, po)
+    @requester = requisition.requester
+    @requisition = requisition
+    @po = po
+    mail(to: %("#{@requester.name}" <#{@requester.email}>), subject: "Your purchase request (#{requisition.id}) has been processed.")
+  end
+end
