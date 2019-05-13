a81788d6259f2ec4b7ef3e9410d67c7688416359
Setiadi <setiadi@cahayabangsa.org>
2019-05-13 14:59:27 +0700

    report sort modifications

--- app/models/diknas_course.rb
+++ app/models/diknas_course.rb
@@ -1,3 +1,15 @@
 class DiknasCourse < ActiveRecord::Base
   has_many :diknas_conversions
+
+
+  def self.grade_option(grade)
+    case grade
+    when 10
+      DiknasCourse.select('*,ipa10 as ipa, ips10 as ips')
+    when 11
+      DiknasCourse.select('*,ipa11 as ipa, ips11 as ips')
+    else
+      DiknasCourse.select('*,ipa12 as ipa, ips12 as ips')
+    end
+  end
 end
