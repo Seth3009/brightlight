8ead8a6019d7f85c99e118fb5189934b4350a206
Medhi Widjaja <medhi.widjaja@cahayabangsa.org>
2019-05-14 12:23:09 +0700

    Update active loan using return_status instead of return_date

--- app/models/book_copy.rb
+++ app/models/book_copy.rb
@@ -123,7 +123,7 @@ class BookCopy < ActiveRecord::Base
   end
 
   def active_loan
-    BookLoan.where(book_copy_id: self.id, return_date: nil)
+    BookLoan.where(book_copy_id: self.id, return_status: nil)
       .order(academic_year_id: :desc, created_at: :desc)
       .includes([:employee, :student])
       .take
