9bc02aa4c474bbda6052a12c3088bbd32e9e9ed5
Medhi Widjaja <medhi.widjaja@cahayabangsa.org>
2019-05-29 11:39:15 +0700

    Exclude disposed books from copy count in Book Edition

--- app/models/book_edition.rb
+++ app/models/book_edition.rb
@@ -249,7 +249,7 @@ group by s.id, s.name order by s.id) f full outer join statuses ss on ss.id = f.
   end
 
   scope :with_number_of_copies, lambda { 
-    joins('LEFT JOIN book_copies ON book_copies.book_edition_id = book_editions.id')
+    joins('LEFT JOIN book_copies ON book_copies.book_edition_id = book_editions.id AND (book_copies.disposed = false OR book_copies.disposed is null)')
     .select('book_editions.id,title,subtitle,description,google_book_id,isbndb_id,isbn10,isbn13,refno,currency,price,authors,publisher,published_date,page_count,language,edition_info,tags,subjects,small_thumbnail,thumbnail,book_title_id, count(book_copies.id) as copies')
     .group('book_editions.id,title,subtitle,description,google_book_id,isbndb_id,isbn10,isbn13,refno,currency,price,authors,publisher,published_date,page_count,language,edition_info,tags,subjects,small_thumbnail,thumbnail,book_title_id')
   }
