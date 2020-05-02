3df726a4481bd266a96950eeacc591e2385f5892
Medhi Widjaja <medhiwidjaja@yahoo.com>
2020-05-02 18:23:16 +0700

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -69,6 +69,23 @@ ActiveRecord::Schema.define(version: 20200501082854) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "activities", force: :cascade do |t|
+    t.integer  "trackable_id"
+    t.string   "trackable_type"
+    t.integer  "owner_id"
+    t.string   "owner_type"
+    t.string   "key"
+    t.text     "parameters"
+    t.integer  "recipient_id"
+    t.string   "recipient_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type", using: :btree
+  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type", using: :btree
+  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type", using: :btree
+
   create_table "activity_schedules", force: :cascade do |t|
     t.string   "activity"
     t.date     "start_date"
